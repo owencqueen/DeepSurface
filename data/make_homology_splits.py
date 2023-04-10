fd74e192bcc31bc62550b8af0852cbb3ed8e5d2c
Owen Queen <oqueen@clr0733.cn.isaac.utk.edu>
2023-04-10 10:41:45 -0400

    Change splits to 5-fold

--- data/make_homology_splits.py
+++ data/make_homology_splits.py
@@ -16,21 +16,21 @@ def main():
     ind_lists = []
 
     for _ in range(5):
-        all_inds = list(KFold(n_splits = 10, shuffle = True, random_state = 1234).split(inds))
+        all_inds = list(KFold(n_splits = 5, shuffle = True, random_state = 1234).split(inds))
         ind_lists.append(all_inds)
 
     pickle.dump(ind_lists, open('splits.pkl', 'wb'))
 
 def test_load():
     inds = pickle.load(open('splits.pkl', 'rb'))
-    print(len(inds))
+    print('len', len(inds))
 
 def example_load_data():
     dpath = '/lustre/isaac/proj/UTK0196/deep-surface-protein-data/M0059E_training_set.tsv'
     df = pd.read_csv(dpath)
 
     # Load one split:
-    split_no = 1
+    split_no = 0
     inds = pickle.load(open('splits.pkl', 'rb'))
 
     for train_inds, test_inds in inds[split_no]:
@@ -38,12 +38,18 @@ def example_load_data():
         test_set = df.iloc[test_inds,:]
         train_seqs_list = train_set['surf.sequence'].tolist() + train_set['deep.sequence'].tolist()
         train_seqs_labels = np.concatenate([np.zeros(train_set.shape[0]), np.ones(train_set.shape[0])])
-        print('list', len(train_seqs_list))
-        print('labels', train_seqs_labels.shape)
+        print('train list', len(train_seqs_list))
+        print('train labels', train_seqs_labels.shape)
+
+        test_seqs_list = test_set['surf.sequence'].tolist() + test_set['deep.sequence'].tolist()
+        test_seqs_labels = np.concatenate([np.zeros(test_set.shape[0]), np.ones(test_set.shape[0])])
+        print('test list', len(test_seqs_list))
+        print('test labels', test_seqs_labels.shape)
+        print('-' * 30)
         
 
 
 if __name__ == '__main__':
     #main()
-    #test_load()
+    test_load()
     example_load_data()
